736c7e84f0d00d1a2e165940af1f40efbcd877f4
wyruo <205507181@qq.com>
2019-04-27 17:46:52 +0800

    day27

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
-	$(".website").click(function(){
+	$(".website").click(function(){/*黑色顶部下拉框  集团网站*/
 		$(".websiteContainer").slideToggle("slow");
+		$(".electContainer").slideUp();
 	});
-	$(".elect").click(function(){
+	$(".elect").click(function(){/*黑色顶部下拉框  选择区域/语言*/
 		$(".electContainer").slideToggle("slow");
+		$(".websiteContainer").slideUp();
 	});
-	$(".searchOpen").click(function(){
- 
-	})
+	$(".searchOpen").click(function(){/*白色顶部 搜索框部分*/
+		$(".searchOpen").hide();
+		$(".whiteTopCenter").hide();
+		$(".searchClose").show();
+	});
+	$(".guan").click(function(){/*白色顶部 搜索框部分*/
+		$(".searchClose").hide();
+		$(".searchOpen").show();
+		$(".whiteTopCenter").show();
+	});
+	$(".searchSuo-search").click(function(){/*缩放后 搜索图标 */
+		$(".hw").hide();
+		$(".searchSuo").hide();
+		$(".searchClose").show();
+	});
+	$(".searchSuo-center").click(function(){
+		
+	});
 });
